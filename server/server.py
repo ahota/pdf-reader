4b8cfaac144e5b7e9d162af0c1cb0ce2232654d7
Alok Hota <ahota@vols.utk.edu>
2018-01-25 17:26:58 -0600

    server finds images in pdf and extracts watermarks
    Found and fixed a bug in goldfish as a result of working on this.
    Goldfish will attempt to extract a watermark from all images, but it is
    not yet known if the data extracted is valid

--- server/server.py
+++ server/server.py
@@ -5,6 +5,9 @@ import minecart
 import os
 import sys
 
+sys.path.append('goldfish')
+from goldfish.watermarker import Watermarker
+
 UPLOAD_DIR = 'pdf_uploads'
 if not os.path.exists(UPLOAD_DIR):
     os.mkdir(UPLOAD_DIR)
@@ -20,6 +23,7 @@ def index():
         if 'file' not in request.files:
             return 'no file given'
         result = _get_upload(request.files['file'])
+        sys.stdout.flush()
         return 'thanks for the file'
 
 def _file_allowed(filename):
@@ -30,16 +34,61 @@ def _file_allowed(filename):
     base_filename, extension = os.path.splitext(filename)
     return extension and extension.lower() == '.pdf'
 
+def _save_file(infile):
+    filename = secure_filename(infile.filename)
+    save_path = os.path.join(app.config['UPLOAD_DIR'], filename)
+    infile.save(save_path)
+
+def _find_images(infile):
+    document = minecart.Document(infile)
+    if document is None:
+        print 'the document is None!'
+        sys.stdout.flush()
+        return 'document was None'
+    images = []
+    for page_num, page in enumerate(document.iter_pages()):
+        for i in page.images:
+            image_info = {}
+            try:
+                image_info['image_data'] = i.as_pil()
+            except ValueError as e:
+                print 'Got a ValueError, skipping'
+            else:
+                image_info['bbox'] = i.get_bbox()
+                image_info['page'] = page_num
+                image_info['message'] = ''
+                image_info['valid'] = False
+                image_info['data'] = ''
+                images.append(image_info)
+
+    print images
+    print len(images), 'images found'
+    sys.stdout.flush()
+
+    return images
+
+def _find_watermarks(images):
+    wm = Watermarker(chan='R', bits_per_pixel=4)
+    candidates = 0
+    for image_info in images:
+        image_info['message'] = wm.extract(image_info['image_data'],
+                message_length=600)
+        candidates += 1
+
+    print images
+    print candidates, 'candidates found'
+    sys.stdout.flush()
+
 def _get_upload(infile):
     if infile and _file_allowed(infile.filename):
-        # get and save the pdf
-        filename = secure_filename(infile.filename)
-        save_path = os.path.join(app.config['UPLOAD_DIR'], filename)
-        infile.save(save_path)
+        # get and save the pdf (may not be necessary)
+        _save_file(infile)
 
         # use minecart to extract images from the pdf
+        images = _find_images(infile)
 
         # for each image, use a goldfish extractor to get a watermark
+        _find_watermarks(images)
 
         # check each watermark against the db
 
