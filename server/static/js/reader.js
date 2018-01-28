1974b63633c246fc217b5d2655f31f3adf6cbcfb
Alok Hota <ahota@vols.utk.edu>
2018-01-28 16:49:58 -0600

    preparing for multi-page pdf rendering
    Moved the overlay-box code to its own function and included a new PDF
    that has multiple pages.
    
    PDF naming convention is <name>_<n_pages>_<n_images>.pdf

--- server/static/js/reader.js
+++ server/static/js/reader.js
@@ -40,44 +40,7 @@ $(document).ready(function() {
             var renderTask = page.render(renderContext);
             renderTask.then(function () {
                 console.log('Page rendered');
-                var overlay = document.getElementById('content');
-                overlay.style.height = canvas.height.toString() + "px";
-                overlay.style.width = canvas.width.toString() + "px";
-                overlay.style.margin = "10px auto";
-                $.ajax({
-                    type: "GET",
-                    url: "/overlay_info",
-                    success: function(result) {
-                        overlay_data = $.parseJSON(result);
-                        console.log(overlay_data);
-                        var box = document.createElement("div");
-                        box.setAttribute("class", "overlay-box");
-                        box.style.left = `${overlay_data[0].bbox[0]}px`;
-                        box.style.bottom = `${overlay_data[0].bbox[1]}px`;
-                        box.style.width = `${overlay_data[0].bbox[2] - overlay_data[0].bbox[0]}px`;
-                        box.style.height = `${overlay_data[0].bbox[3] - overlay_data[0].bbox[1]}px`;
-                        box.setAttribute("data-intro", overlay_data[0].data);
-                        if(overlay_data[0].bbox[0] < (viewport.width / 2)) {
-                            box.setAttribute("data-position", "left");
-                        }
-                        else {
-                            box.setAttribute("data-position", "right");
-                        }
-                        box.setAttribute("data-toggle", "chardinjs");
-                        //var box_data = document.createElement("div");
-                        //box_data.setAttribute("class", "overlay-box-data");
-                        //box_data.innerHTML = overlay_data[0].data;
-                        //box.appendChild(box_data);
-                        overlay.appendChild(box);
-                        $('.overlay-box').on("click", function(ev) {
-                            ev.preventDefault();
-                            console.log("hey");
-                            $('body').chardinJs('toggle');
-                        });
-                    },
-                    error: function(result) {
-                    }
-                });
+                setupOverlay(canvas);
             });
 
         });
@@ -85,4 +48,45 @@ $(document).ready(function() {
         // PDF loading error
         console.error(reason);
     });
+
+    function setupOverlay(canvas) {
+        var overlay = document.getElementById('content');
+        overlay.style.height = canvas.height.toString() + "px";
+        overlay.style.width = canvas.width.toString() + "px";
+        overlay.style.margin = "10px auto";
+        $.ajax({
+            type: "GET",
+            url: "/overlay_info",
+            success: function(result) {
+                overlay_data = $.parseJSON(result);
+                console.log(overlay_data);
+                var box = document.createElement("div");
+                box.setAttribute("class", "overlay-box");
+                box.style.left = `${overlay_data[0].bbox[0]}px`;
+                box.style.bottom = `${overlay_data[0].bbox[1]}px`;
+                box.style.width = `${overlay_data[0].bbox[2] - overlay_data[0].bbox[0]}px`;
+                box.style.height = `${overlay_data[0].bbox[3] - overlay_data[0].bbox[1]}px`;
+                box.setAttribute("data-intro", overlay_data[0].data);
+                if(overlay_data[0].bbox[0] < (viewport.width / 2)) {
+                    box.setAttribute("data-position", "left");
+                }
+                else {
+                    box.setAttribute("data-position", "right");
+                }
+                box.setAttribute("data-toggle", "chardinjs");
+                //var box_data = document.createElement("div");
+                //box_data.setAttribute("class", "overlay-box-data");
+                //box_data.innerHTML = overlay_data[0].data;
+                //box.appendChild(box_data);
+                overlay.appendChild(box);
+                $('.overlay-box').on("click", function(ev) {
+                    ev.preventDefault();
+                    console.log("hey");
+                    $('body').chardinJs('toggle');
+                });
+            },
+            error: function(result) {
+            }
+        });
+    }
 });
