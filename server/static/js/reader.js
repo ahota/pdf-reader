a61f29a98e22dbbe1217a142eca423ad0bea2b29
Alok Hota <ahota@vols.utk.edu>
2018-01-28 14:18:37 -0600

    centered page in document
    Chardin tooltip shows up on either left or right depending on image left
    margin

--- server/static/js/reader.js
+++ server/static/js/reader.js
@@ -40,46 +40,46 @@ $(document).ready(function() {
             var renderTask = page.render(renderContext);
             renderTask.then(function () {
                 console.log('Page rendered');
-                $('body').on("click", function(ev) {
-                    ev.preventDefault();
-                    console.log("hey");
-                    $('body').chardinJs('start');
+                var overlay = document.getElementById('content');
+                overlay.style.height = canvas.height.toString() + "px";
+                overlay.style.width = canvas.width.toString() + "px";
+                overlay.style.margin = "10px auto";
+                $.ajax({
+                    type: "GET",
+                    url: "/overlay_info",
+                    success: function(result) {
+                        overlay_data = $.parseJSON(result);
+                        console.log(overlay_data);
+                        var box = document.createElement("div");
+                        box.setAttribute("class", "overlay-box");
+                        box.style.left = `${overlay_data[0].bbox[0]}px`;
+                        box.style.bottom = `${overlay_data[0].bbox[1]}px`;
+                        box.style.width = `${overlay_data[0].bbox[2] - overlay_data[0].bbox[0]}px`;
+                        box.style.height = `${overlay_data[0].bbox[3] - overlay_data[0].bbox[1]}px`;
+                        box.setAttribute("data-intro", overlay_data[0].data);
+                        if(overlay_data[0].bbox[0] < (viewport.width / 2)) {
+                            box.setAttribute("data-position", "left");
+                        }
+                        else {
+                            box.setAttribute("data-position", "right");
+                        }
+                        box.setAttribute("data-toggle", "chardinjs");
+                        //var box_data = document.createElement("div");
+                        //box_data.setAttribute("class", "overlay-box-data");
+                        //box_data.innerHTML = overlay_data[0].data;
+                        //box.appendChild(box_data);
+                        overlay.appendChild(box);
+                        $('.overlay-box').on("click", function(ev) {
+                            ev.preventDefault();
+                            console.log("hey");
+                            $('body').chardinJs('toggle');
+                        });
+                    },
+                    error: function(result) {
+                    }
                 });
             });
 
-            var overlay = document.getElementById('content');
-            overlay.height = canvas.height;
-            overlay.width = canvas.width;
-            overlay.style.margin = "10px auto";
-            $.ajax({
-                type: "GET",
-                url: "/overlay_info",
-                success: function(result) {
-                    overlay_data = $.parseJSON(result);
-                    console.log(overlay_data);
-                    var box = document.createElement("div");
-                    box.setAttribute("class", "overlay-box");
-                    box.style.left = `${overlay_data[0].bbox[0]}px`;
-                    box.style.bottom = `-${viewport.height - overlay_data[0].bbox[1]}px`;
-                    box.style.width = `${overlay_data[0].bbox[2] - overlay_data[0].bbox[0]}px`;
-                    box.style.height = `${overlay_data[0].bbox[3] - overlay_data[0].bbox[1]}px`;
-                    box.setAttribute("data-intro", overlay_data[0].data);
-                    if(overlay_data[0].bbox[0] < (viewport.width / 2)) {
-                        box.setAttribute("data-position", "right");
-                    }
-                    else {
-                        box.setAttribute("data-position", "right");
-                    }
-                    box.setAttribute("data-toggle", "chardinjs");
-                    //var box_data = document.createElement("div");
-                    //box_data.setAttribute("class", "overlay-box-data");
-                    //box_data.innerHTML = overlay_data[0].data;
-                    //box.appendChild(box_data);
-                    overlay.appendChild(box);
-                },
-                error: function(result) {
-                }
-            });
         });
     }, function (reason) {
         // PDF loading error
