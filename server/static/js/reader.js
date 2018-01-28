8045ccf6eeb27a610e2f32a3bdc86c0bb2d8036c
Alok Hota <ahota@vols.utk.edu>
2018-01-28 17:20:27 -0600

    multi-page rendering
    No overlays at the moment

--- server/static/js/reader.js
+++ server/static/js/reader.js
@@ -1,92 +1,93 @@
 overlay_data = null;
 
-$(document).ready(function() {
-    // If absolute URL from the remote server is provided, configure the CORS
-    // header on that server.
-    var url = '/pdfs/last.pdf';
+// based on: https://gist.github.com/fcingolani/3300351
+function renderPDF(url, container, scale) {
 
-    // Disable workers to avoid yet another cross-origin issue (workers need
-    // the URL of the script to be loaded, and dynamically loading a cross-origin
-    // script does not work).
-    // PDFJS.disableWorker = true;
+    if(!scale)
+        var scale = 1.5;
 
-    // The workerSrc property shall be specified.
-    PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+    // render a single page from the PDF into its own canvas,
+    // then append that canvas to the container
+    // This means that the HTML only needs a container div and
+    // no canvas elements
+    function renderPage(page) {
+        console.log(page);
+        var viewport = page.getViewport(scale);
+        var canvas = document.createElement('canvas');
+        var context = canvas.getContext('2d');
+        var renderContext = {
+            canvasContext: context,
+            viewport: viewport
+        };
 
-    // Asynchronous download of PDF
-    var loadingTask = PDFJS.getDocument(url);
-    loadingTask.promise.then(function(pdf) {
-        console.log('PDF loaded');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
 
-        // Fetch the first page
-        var pageNumber = 1;
-        pdf.getPage(pageNumber).then(function(page) {
-            console.log('Page loaded');
+        // set the container width so that the pages don't render side-by-side
+        if(page.pageIndex == 0) {
+            container.style.height = canvas.height.toString() + "px";
+            container.style.width = canvas.width.toString() + "px";
+            container.style.margin = "10px auto";
+        }
 
-            var scale = 1.0;
-            var viewport = page.getViewport(scale);
+        container.appendChild(canvas);
 
-            // Prepare canvas using PDF page dimensions
-            var canvas = document.getElementById('the-canvas');
-            var context = canvas.getContext('2d');
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-
-            // Render PDF page into canvas context
-            var renderContext = {
-                canvasContext: context,
-                viewport: viewport
-            };
-            var renderTask = page.render(renderContext);
-            renderTask.then(function () {
-                console.log('Page rendered');
-                setupOverlay(canvas);
-            });
-
-        });
-    }, function (reason) {
-        // PDF loading error
-        console.error(reason);
-    });
-
-    function setupOverlay(canvas) {
-        var overlay = document.getElementById('content');
-        overlay.style.height = canvas.height.toString() + "px";
-        overlay.style.width = canvas.width.toString() + "px";
-        overlay.style.margin = "10px auto";
-        $.ajax({
-            type: "GET",
-            url: "/overlay_info",
-            success: function(result) {
-                overlay_data = $.parseJSON(result);
-                console.log(overlay_data);
-                var box = document.createElement("div");
-                box.setAttribute("class", "overlay-box");
-                box.style.left = `${overlay_data[0].bbox[0]}px`;
-                box.style.bottom = `${overlay_data[0].bbox[1]}px`;
-                box.style.width = `${overlay_data[0].bbox[2] - overlay_data[0].bbox[0]}px`;
-                box.style.height = `${overlay_data[0].bbox[3] - overlay_data[0].bbox[1]}px`;
-                box.setAttribute("data-intro", overlay_data[0].data);
-                if(overlay_data[0].bbox[0] < (viewport.width / 2)) {
-                    box.setAttribute("data-position", "left");
-                }
-                else {
-                    box.setAttribute("data-position", "right");
-                }
-                box.setAttribute("data-toggle", "chardinjs");
-                //var box_data = document.createElement("div");
-                //box_data.setAttribute("class", "overlay-box-data");
-                //box_data.innerHTML = overlay_data[0].data;
-                //box.appendChild(box_data);
-                overlay.appendChild(box);
-                $('.overlay-box').on("click", function(ev) {
-                    ev.preventDefault();
-                    console.log("hey");
-                    $('body').chardinJs('toggle');
-                });
-            },
-            error: function(result) {
-            }
-        });
+        page.render(renderContext);
     }
+
+    // iteratively render all the pages in the PDF
+    function renderPages(doc) {
+        // 1-based indexing >_>
+        for(var p = 1; p <= doc.numPages; p++)
+            doc.getPage(p).then(renderPage);
+    }
+
+    PDFJS.disableWorker = true;
+    PDFJS.getDocument(url).then(renderPages);
+}
+
+function setupOverlay(canvas) {
+    var overlay = document.getElementById('content');
+    overlay.style.height = canvas.height.toString() + "px";
+    overlay.style.width = canvas.width.toString() + "px";
+    overlay.style.margin = "10px auto";
+    $.ajax({
+        type: "GET",
+        url: "/overlay_info",
+        success: function(result) {
+            overlay_data = $.parseJSON(result);
+            console.log(overlay_data);
+            var box = document.createElement("div");
+            box.setAttribute("class", "overlay-box");
+            box.style.left = `${overlay_data[0].bbox[0]}px`;
+            box.style.bottom = `${overlay_data[0].bbox[1]}px`;
+            box.style.width = `${overlay_data[0].bbox[2] - overlay_data[0].bbox[0]}px`;
+            box.style.height = `${overlay_data[0].bbox[3] - overlay_data[0].bbox[1]}px`;
+            box.setAttribute("data-intro", overlay_data[0].data);
+            if(overlay_data[0].bbox[0] < (viewport.width / 2)) {
+                box.setAttribute("data-position", "left");
+            }
+            else {
+                box.setAttribute("data-position", "right");
+            }
+            box.setAttribute("data-toggle", "chardinjs");
+            //var box_data = document.createElement("div");
+            //box_data.setAttribute("class", "overlay-box-data");
+            //box_data.innerHTML = overlay_data[0].data;
+            //box.appendChild(box_data);
+            overlay.appendChild(box);
+            $('.overlay-box').on("click", function(ev) {
+                ev.preventDefault();
+                console.log("hey");
+                $('body').chardinJs('toggle');
+            });
+        },
+        error: function(result) {
+        }
+    });
+}
+
+$(document).ready(function() {
+    var url = '/pdfs/last.pdf';
+    renderPDF(url, document.getElementById('content'));
 });
